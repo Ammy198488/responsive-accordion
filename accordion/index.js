78726c37c9bd465657b365be0479cc49030a3299
ammy <74745072+Ammy198488@users.noreply.github.com>
2022-03-08 14:31:47 +0100

    Add files via upload

---
+++ accordion/index.js
@@ -0,0 +1,9 @@
+let accordions = document.querySelectorAll('.accordion-container .accordion');
+accordions.forEach(acco => {
+    acco.onclick = () =>{
+        accordions.forEach(subAcco => {
+            subAcco.classList.remove('active')
+        })
+        acco.classList.add('active');
+    }
+})
